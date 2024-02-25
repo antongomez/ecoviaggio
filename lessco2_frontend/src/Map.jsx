dd0d28cd2cc6e95769f3985afae2ab62337d8b85
antongomez <anton.gomez.lopez@rai.usc.es>
2024-02-25 18:20:43 +0100

    Adjust Zoom on Results page and add Marker to public transport route

---
+++ lessco2_frontend/src/Map.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import "leaflet-routing-machine";
+import L from "leaflet";
+import { createControlComponent } from "@react-leaflet/core";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Polyline,
+  Marker,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+
+const createRouting = (props) => {
+  const instance = L.Routing.control({
+    waypoints: props.coordinates_group,
+
+    show: false,
+    addWaypoints: false,
+    draggableWaypoints: false,
+    fitSelectedRoutes: false,
+    showAlternatives: false,
+  });
+
+  return instance;
+};
+
+const RoutingMachine = createControlComponent(createRouting);
+
+export const Map = ({
+  containerStyle,
+  coordinate_car_routing,
+  coordinate_public_cars,
+  coordinate_public_planes,
+  coordinate_public_trains,
+  first_stop,
+  last_stop,
+}) => {
+  return (
+    <MapContainer scrollWheelZoom={true} style={containerStyle}>
+      <AttributionControl />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <InnerMapComponent
+        {...{
+          coordinate_car_routing,
+          coordinate_public_cars,
+          coordinate_public_planes,
+          coordinate_public_trains,
+          first_stop,
+          last_stop,
+        }}
+      />
+    </MapContainer>
+  );
+};
+
+const getCoordinatesFromRoute = (coordinates) => {
+  const points = [];
+  coordinates.forEach((pair_origin_dest) => {
+    pair_origin_dest.forEach((coordinates) => {
+      points.push(L.latLng(coordinates));
+    });
+  });
+  return points;
+};
+
+const InnerMapComponent = ({
+  coordinate_car_routing,
+  coordinate_public_cars,
+  coordinate_public_planes,
+  coordinate_public_trains,
+  first_stop,
+  last_stop,
+}) => {
+  // Ajusta los límites del mapa cuando punto1 o punto2 cambian
+  if (
+    coordinate_car_routing ||
+    coordinate_public_trains ||
+    coordinate_public_planes ||
+    coordinate_public_cars
+  ) {
+    const public_transport_routes = [
+      coordinate_public_cars,
+      coordinate_public_planes,
+      coordinate_public_trains,
+    ];
+    // Color to use for each public transport on the map
+    const colors = ["red", "blue", "purple"];
+
+    // Get all the points from the routes to adjust the limits of the map
+    const points = [];
+    if (
+      coordinate_car_routing !== undefined &&
+      coordinate_car_routing.length > 0
+    ) {
+      points.push(...getCoordinatesFromRoute(coordinate_car_routing));
+    }
+    public_transport_routes.forEach((route) => {
+      if (route !== undefined && route.length > 0) {
+        points.push(...getCoordinatesFromRoute(route));
+      }
+    });
+
+    if (first_stop) console.log(first_stop);
+
+    // Adjust the map to the points
+    const map = useMap();
+    const limits = L.latLngBounds(points); // Establish the points that should be visible
+    map.fitBounds(limits.pad(0.1)); // Add a padding to the limits (in percentage of the map's size)
+
+    return (
+      <>
+        {coordinate_car_routing &&
+          coordinate_car_routing.map((coordinates_group, k) => {
+            return (
+              <RoutingMachine coordinates_group={coordinates_group} key={k} />
+            );
+          })}
+        {first_stop && <Marker position={L.latLng(first_stop)} />}
+        {last_stop && <Marker position={L.latLng(last_stop)} />}
+        {public_transport_routes.map((route, k) => {
+          if (route === undefined || route.length === 0) {
+            return null;
+          }
+          return (
+            <Polyline
+              pathOptions={{ color: colors[k] }}
+              positions={route}
+              key={k}
+            />
+          );
+        })}
+      </>
+    );
+  }
+
+  return null;
+};
+
+function AttributionControl() {
+  const map = useMap();
+
+  map.attributionControl.setPrefix(
+    '<a href="https://leafletjs.com/" title="A JavaScript library for interactive maps">Leaflet</a>'
+  );
+
+  return null;
+}
--- lessco2_frontend/src/Results.jsx
+++ lessco2_frontend/src/Results.jsx
@@ -3,9 +3,8 @@
 // SPDX-License-Identifier: MIT
 
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
-import { Col, Row } from "react-bootstrap";
 import {
   CarFront,
   ArrowRight,
@@ -16,12 +15,7 @@ import {
   FuelPump,
 } from "react-bootstrap-icons";
 
-import "leaflet-routing-machine";
-import L from "leaflet";
-import { createControlComponent } from "@react-leaflet/core";
-import { MapContainer, TileLayer, useMap, Polyline } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import { Map } from "./Map";
 
 export const Results = () => {
   const { state } = useLocation();
@@ -38,41 +32,71 @@ export const Results = () => {
         </Col>
       </Row>
       {state.map((route, index) => {
-        return <GroupRoute route={route} index={index} key={index} className="mb-5" />;
+        return (
+          <GroupRoute
+            route={route}
+            index={index}
+            key={index}
+            className="mb-5"
+          />
+        );
       })}
     </Container>
   );
 };
 
 const containerStyle = {
-  width: "100 %",
+  width: "100%",
   height: "350px",
 };
 
-const createRouting = (props) => {
-  const instance = L.Routing.control({
-    waypoints: props.coordinates_group,
+function getPolylinesCoordinates(step2) {
+  let polyline_trains = [];
+  let polyline_planes = [];
+  let polyline_cars = [];
 
-    show: false,
-    addWaypoints: false,
-    draggableWaypoints: false,
-    fitSelectedRoutes: false,
-    showAlternatives: false,
+  step2["path"].map((step) => {
+    if (step["travel_type"] === "TRAIN") {
+      polyline_trains.push([
+        [
+          step["origin_coordinates"]["latitude"],
+          step["origin_coordinates"]["longitude"],
+        ],
+        [
+          step["destiny_coordinates"]["latitude"],
+          step["destiny_coordinates"]["longitude"],
+        ],
+      ]);
+    } else if (step["travel_type"] === "CAR") {
+      polyline_cars.push([
+        [
+          step["origin_coordinates"]["latitude"],
+          step["origin_coordinates"]["longitude"],
+        ],
+        [
+          step["destiny_coordinates"]["latitude"],
+          step["destiny_coordinates"]["longitude"],
+        ],
+      ]);
+    } else {
+      polyline_planes.push([
+        [
+          step["origin_coordinates"]["latitude"],
+          step["origin_coordinates"]["longitude"],
+        ],
+        [
+          step["destiny_coordinates"]["latitude"],
+          step["destiny_coordinates"]["longitude"],
+        ],
+      ]);
+    }
   });
 
-  return instance;
-};
-
-const Routing = createControlComponent(createRouting);
-
-function AttributionControl() {
-  const map = useMap();
-
-  map.attributionControl.setPrefix(
-    '<a href="https://leafletjs.com/" title="A JavaScript library for interactive maps">Leaflet</a>'
-  );
-
-  return null;
+  return {
+    polyline_trains,
+    polyline_planes,
+    polyline_cars,
+  };
 }
 
 function GroupRoute({ route, index }) {
@@ -89,63 +113,26 @@ function GroupRoute({ route, index }) {
     step1_emissions +
     (step2 === undefined ? 0 : step2["emissions"] + step3["emissions"]);
 
-  let center = undefined;
-
-  let coordinates = [];
+  let coordinate_car_routing_origin = [];
   step1.map((group) => {
     let coordinates_group = [];
     group["path"].map((step) => {
-      if (center === undefined) {
-        center = L.latLng(step[1], step[2]);
-      }
       coordinates_group.push(L.latLng(step[1], step[2]));
     });
-    coordinates.push(coordinates_group);
+    coordinate_car_routing_origin.push(coordinates_group);
   });
 
   let polyline_trains = [];
   let polyline_planes = [];
   let polyline_cars = [];
 
-  let overall_coordinates = [...coordinates];
+  let coordinate_car_routing = [...coordinate_car_routing_origin];
   if (step2 !== undefined) {
-    step2["path"].map((step) => {
-      if (step["travel_type"] === "TRAIN") {
-        polyline_trains.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      } else if (step["travel_type"] === "CAR") {
-        polyline_cars.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      } else {
-        polyline_planes.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      }
-    });
-    overall_coordinates.push([
+    // Get the coordinates for the public transport section
+    ({ polyline_trains, polyline_planes, polyline_cars } =
+      getPolylinesCoordinates(step2));
+    // Get the coordinates for the last car section
+    coordinate_car_routing.push([
       L.latLng(step3["origin"]["latitude"], step3["origin"]["longitude"]),
       L.latLng(step3["destination"][1], step3["destination"][2]),
     ]);
@@ -159,62 +146,40 @@ function GroupRoute({ route, index }) {
         <Row>
           <Col lg={6}>
             {step1.map((group, k) => {
-              return <FriendsPath key = {k} group={group} emissions={step1_emissions} />;
+              return (
+                <FriendsPath
+                  key={k}
+                  group={group}
+                  emissions={step1_emissions}
+                />
+              );
             })}
           </Col>
-          <Col lg={4} className="px-0">
-            <MapContainer
-              center={coordinates[0][0]}
-              zoom={8}
-              scrollWheelZoom={true}
-              style={containerStyle}
-            >
-              <AttributionControl />
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {coordinates.map((coordinates_group, k) => {
-                return <Routing coordinates_group={coordinates_group} key={k}/>;
-              })}
-            </MapContainer>
+          <Col lg={5} className="px-0">
+            <Map
+              containerStyle={containerStyle}
+              coordinate_car_routing={coordinate_car_routing_origin}
+            />
           </Col>
         </Row>
-      </Col>
-      <OptionalPath step2={step2} step3={step3} />
-      <hr className="my-4" />
-      <Col>
-        <h3>Full Trip</h3>
-        <FuelComsumption travel_step={4} emissions={total_emissions} />
-        <Row className="d-flex justify-content-center">
-          <Col lg={8}>
-            <MapContainer
-              center={center}
-              zoom={6}
-              scrollWheelZoom={true}
-              style={containerStyle}
-            >
-              <AttributionControl />
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Polyline
-                pathOptions={{ color: "purple" }}
-                positions={polyline_trains}
-              />
-              <Polyline
-                pathOptions={{ color: "blue" }}
-                positions={polyline_planes}
-              />
-              <Polyline
-                pathOptions={{ color: "red" }}
-                positions={polyline_cars}
-              />
-              {overall_coordinates.map((coordinates_group, k) => {
-                return <Routing coordinates_group={coordinates_group} key={k}/>;
-              })}
-            </MapContainer>
+
+        <OptionalPath step2={step2} step3={step3} />
+        <hr className="my-4" />
+        <Row>
+          <Col>
+            <h3>Full Trip</h3>
+            <FuelComsumption travel_step={4} emissions={total_emissions} />
+            <Row className="d-flex justify-content-center">
+              <Col lg={8}>
+                <Map
+                  containerStyle={containerStyle}
+                  coordinate_car_routing={coordinate_car_routing}
+                  coordinate_public_trains={polyline_trains}
+                  coordinate_public_planes={polyline_planes}
+                  coordinate_public_cars={polyline_cars}
+                />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Col>
@@ -222,7 +187,7 @@ function GroupRoute({ route, index }) {
   );
 }
 
-function TravelSubstep ({origin, destination, travel_type}) {
+function TravelSubstep({ origin, destination, travel_type }) {
   let travel_type_icon = <></>;
   switch (travel_type) {
     case "TRAIN":
@@ -236,30 +201,30 @@ function TravelSubstep ({origin, destination, travel_type}) {
       break;
   }
 
-  return <Row className="py-1">
-          <Col xs="auto">
-            {travel_type_icon}
-          </Col>
-          <Col xs="auto" className="px-1">
-            <GeoAltFill />
-          </Col>
-          <Col xs="auto" className="pe-2 ps-0">
-            <span>{origin}</span>
-          </Col>
-          <Col xs="auto" className="px-2">
-            <ArrowRight />
-          </Col>
-          <Col xs="auto" className="px-1">
-            <GeoAltFill />
-          </Col>
-          <Col xs="auto" className="pe-2 ps-0">
-            <span>{destination}</span>
-          </Col>
-        </Row>
+  return (
+    <Row className="py-1">
+      <Col xs="auto">{travel_type_icon}</Col>
+      <Col xs="auto" className="px-1">
+        <GeoAltFill />
+      </Col>
+      <Col xs="auto" className="pe-2 ps-0">
+        <span>{origin}</span>
+      </Col>
+      <Col xs="auto" className="px-2">
+        <ArrowRight />
+      </Col>
+      <Col xs="auto" className="px-1">
+        <GeoAltFill />
+      </Col>
+      <Col xs="auto" className="pe-2 ps-0">
+        <span>{destination}</span>
+      </Col>
+    </Row>
+  );
 }
 
 function FuelComsumption({ travel_step, emissions }) {
-  let emissions_text = ''
+  let emissions_text = "";
   switch (travel_step) {
     case 1:
       emissions_text = "First car section";
@@ -275,16 +240,18 @@ function FuelComsumption({ travel_step, emissions }) {
       break;
   }
 
-  return  <Row className="pt-2">
-    <Col xs="auto" className="py-2">
-      <FuelPump size={25} />
-    </Col>
-    <Col xs="auto" className="py-2">
-      <p>
-        {emissions_text} emissions: {Math.round(emissions * 100) / 100} kg Co2
-      </p>
-    </Col>
-  </Row>
+  return (
+    <Row className="pt-2">
+      <Col xs="auto" className="py-2">
+        <FuelPump size={25} />
+      </Col>
+      <Col xs="auto" className="py-2">
+        <p>
+          {emissions_text} emissions: {Math.round(emissions * 100) / 100} kg Co2
+        </p>
+      </Col>
+    </Row>
+  );
 }
 
 function FriendsPath({ group, emissions }) {
@@ -335,60 +302,28 @@ function OptionalPath({ step2, step3 }) {
   if (step2 === undefined) {
     return null;
   } else {
-    let coordinates_step3 = [
-      L.latLng(step3["origin"]["latitude"], step3["origin"]["longitude"]),
-      L.latLng(step3["destination"][1], step3["destination"][2]),
+    const coordinates_step3 = [
+      [
+        L.latLng(step3["origin"]["latitude"], step3["origin"]["longitude"]),
+        L.latLng(step3["destination"][1], step3["destination"][2]),
+      ],
     ];
 
-    let polyline_trains = [];
-    let polyline_planes = [];
-    let polyline_cars = [];
+    const { polyline_trains, polyline_planes, polyline_cars } =
+      getPolylinesCoordinates(step2);
 
-    let center = undefined;
-
-    step2["path"].map((step) => {
-      if (center === undefined) {
-        center = {
-          lat: step["origin_coordinates"]["latitude"],
-          lng: step["origin_coordinates"]["longitude"],
-        };
-      }
-
-      if (step["travel_type"] === "TRAIN") {
-        polyline_trains.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      } else if (step["travel_type"] === "CAR") {
-        polyline_cars.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      } else {
-        polyline_planes.push([
-          [
-            step["origin_coordinates"]["latitude"],
-            step["origin_coordinates"]["longitude"],
-          ],
-          [
-            step["destiny_coordinates"]["latitude"],
-            step["destiny_coordinates"]["longitude"],
-          ],
-        ]);
-      }
-    });
+    const first_stop = {
+      lat: step2["path"][0]["origin_coordinates"]["latitude"],
+      lng: step2["path"][0]["origin_coordinates"]["longitude"],
+    };
+    const last_stop = {
+      lat: step2["path"][step2["path"].length - 1]["destiny_coordinates"][
+        "latitude"
+      ],
+      lng: step2["path"][step2["path"].length - 1]["destiny_coordinates"][
+        "longitude"
+      ],
+    };
 
     return (
       <>
@@ -398,63 +333,48 @@ function OptionalPath({ step2, step3 }) {
             <h4>Public transport section:</h4>
             <Row>
               {step2["path"].map((step, k) => {
-                return <TravelSubstep key={k} origin={step["origin"]} destination={step["destiny"]} travel_type={step["travel_type"]} />;
+                return (
+                  <TravelSubstep
+                    key={k}
+                    origin={step["origin"]}
+                    destination={step["destiny"]}
+                    travel_type={step["travel_type"]}
+                  />
+                );
               })}
             </Row>
             <FuelComsumption travel_step={2} emissions={step2["emissions"]} />
           </Col>
-          <Col lg={4} className="d-flex flex-column justify-content-center">
-            <MapContainer
-              center={center}
-              zoom={6}
-              scrollWheelZoom={true}
-              style={containerStyle}
-            >
-              <AttributionControl />
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Polyline
-                pathOptions={{ color: "purple" }}
-                positions={polyline_trains}
-              />
-              <Polyline
-                pathOptions={{ color: "blue" }}
-                positions={polyline_planes}
-              />
-              <Polyline
-                pathOptions={{ color: "red" }}
-                positions={polyline_cars}
-              />
-            </MapContainer>
-            ,
+          <Col
+            lg={5}
+            className="d-flex flex-column justify-content-center px-0"
+          >
+            <Map
+              containerStyle={containerStyle}
+              coordinate_public_cars={polyline_cars}
+              coordinate_public_planes={polyline_planes}
+              coordinate_public_trains={polyline_trains}
+              first_stop={first_stop}
+              last_stop={last_stop}
+            />
           </Col>
         </Row>
         <hr className="my-4" />
         <Row>
           <Col lg={6} className="py-2">
             <h4>Last car section:</h4>
-            <TravelSubstep origin={step3["origin"]["name"]} destination={step3["destination"][0]} travel_type="CAR" />
+            <TravelSubstep
+              origin={step3["origin"]["name"]}
+              destination={step3["destination"][0]}
+              travel_type="CAR"
+            />
             <FuelComsumption travel_step={3} emissions={step3["emissions"]} />
           </Col>
-          <Col lg={4}>
-            <MapContainer
-              center={{
-                lat: step3["origin"]["latitude"],
-                lng: step3["origin"]["longitude"],
-              }}
-              zoom={8}
-              scrollWheelZoom={true}
-              style={containerStyle}
-            >
-              <AttributionControl />
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Routing coordinates_group={coordinates_step3} />
-            </MapContainer>
+          <Col lg={5} className="px-0">
+            <Map
+              containerStyle={containerStyle}
+              coordinate_car_routing={coordinates_step3}
+            />
           </Col>
         </Row>
       </>
